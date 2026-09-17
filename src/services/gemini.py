141bf7db30375b6187693ea93778ba0e8392326a
SergeyL566 <lapko97@mail.ru>
2026-09-18 00:09:58 +0300

    Update gemini.py

--- src/services/gemini.py
+++ src/services/gemini.py
@@ -46,8 +46,8 @@ if settings.FORCE_IPV6:
 
 client = genai.Client(
     api_key=settings.GEMINI_API_KEY,
-    base_url=settings.GEMINI_API_BASE,
     http_options=types.HttpOptions(
+        base_url=settings.GEMINI_API_BASE,   # ✅ Сюда
         client_args=client_args,
         async_client_args=async_client_args
     )
